a46c71ea9fe4a3d1a21b10a588082c7f5e026611
KThanaawat <Thanaawat@gmail.com>
2018-12-17 04:57:14 +0700

    Add JavaScript

---
+++ #Apple/apple.js
@@ -0,0 +1,65 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Number of tweets about 'Apple Store' on a #ICONSIAM before Grand opening", 
+        "colorway": [
+            "#fbb4ae", 
+            "#b3cde3", 
+            "#ccebc5", 
+            "#decbe4", 
+            "#fed9a6", 
+            "#ffffcc", 
+            "#e5d8bd", 
+            "#fddaec", 
+            "#f2f2f2"
+        ], 
+        "xaxis": {
+            "range": [
+                -0.5, 
+                8.5
+            ], 
+            "type": "category", 
+            "autorange": true, 
+            "title": "Date"
+        }, 
+        "yaxis": {
+            "range": [
+                0, 
+                5.2631578947368425
+            ], 
+            "autorange": true, 
+            "title": "Number of tweets"
+        }
+    }, 
+    "data": [
+        {
+            "x": [
+                "11/27/2018", 
+                "11/28/2018", 
+                "11/28/2018", 
+                "11/29/2018", 
+                "12/1/2018", 
+                "12/1/2018", 
+                "12/1/2018", 
+                "12/1/2018", 
+                "12/2/2018", 
+                "12/2/2018", 
+                "12/3/2018", 
+                "12/5/2018", 
+                "12/5/2018", 
+                "12/5/2018", 
+                "12/5/2018", 
+                "12/5/2018", 
+                "12/6/2018", 
+                "12/6/2018", 
+                "12/7/2018", 
+                "12/7/2018", 
+                "12/7/2018"
+            ], 
+            "type": "histogram", 
+            "mode": "markers", 
+            "xsrc": "ksankaseam:2:d00d9e"
+        }
+    ]
+}
---
+++ keywords/keywords.js
@@ -0,0 +1,122 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Analysis of customer satisfaction from keywords", 
+        "colorway": [
+            "#fbb4ae", 
+            "#b3cde3", 
+            "#ccebc5", 
+            "#decbe4", 
+            "#fed9a6", 
+            "#ffffcc", 
+            "#e5d8bd", 
+            "#fddaec", 
+            "#f2f2f2"
+        ], 
+        "xaxis": {
+            "range": [
+                -0.5, 
+                9.5
+            ], 
+            "type": "category", 
+            "autorange": true, 
+            "title": "Keywords"
+        }, 
+        "yaxis": {
+            "range": [
+                0, 
+                106.3157894736842
+            ], 
+            "type": "linear", 
+            "autorange": true, 
+            "title": "Number of tweets"
+        }
+    }, 
+    "data": [
+        {
+            "labelssrc": "ksankaseam:6:691c40", 
+            "name": "Good keywords", 
+            "ysrc": "ksankaseam:6:009ec7", 
+            "labels": [
+                "ชอบ", 
+                "ดี", 
+                "สวย", 
+                "ใหญ่", 
+                "สะอาด"
+            ], 
+            "xsrc": "ksankaseam:6:691c40", 
+            "marker": {
+                "colors": []
+            }, 
+            "stackgroup": 1, 
+            "values": [
+                "24", 
+                "101", 
+                "34", 
+                "26", 
+                "1"
+            ], 
+            "mode": "lines", 
+            "y": [
+                "24", 
+                "101", 
+                "34", 
+                "26", 
+                "1"
+            ], 
+            "x": [
+                "ชอบ", 
+                "ดี", 
+                "สวย", 
+                "ใหญ่", 
+                "สะอาด"
+            ], 
+            "type": "bar", 
+            "valuessrc": "ksankaseam:6:009ec7", 
+            "orientation": "v"
+        }, 
+        {
+            "labelssrc": "ksankaseam:6:9531b3", 
+            "name": "Bad keywords", 
+            "ysrc": "ksankaseam:6:a8a063", 
+            "labels": [
+                "ห้องน้ำ", 
+                "เดินทาง", 
+                "ไกล", 
+                "รถติด", 
+                "แพง"
+            ], 
+            "xsrc": "ksankaseam:6:9531b3", 
+            "marker": {
+                "colors": []
+            }, 
+            "stackgroup": null, 
+            "values": [
+                "1", 
+                "6", 
+                "6", 
+                "9", 
+                "4"
+            ], 
+            "mode": "markers", 
+            "y": [
+                "1", 
+                "6", 
+                "6", 
+                "9", 
+                "4"
+            ], 
+            "x": [
+                "ห้องน้ำ", 
+                "เดินทาง", 
+                "ไกล", 
+                "รถติด", 
+                "แพง"
+            ], 
+            "type": "bar", 
+            "valuessrc": "ksankaseam:6:a8a063", 
+            "orientation": "v"
+        }
+    ]
+}
