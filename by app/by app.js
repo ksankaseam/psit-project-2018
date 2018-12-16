069e9575247edbb7c1fda799be04aa93f122b0b0
Thymme <Thymme.T@gmail.com>
2018-12-17 05:07:07 +0700

    Add by app JavaScript

---
+++ by app/by app.js
@@ -0,0 +1,80 @@
+var figure = {
+    "frames": [], 
+    "layout": {
+        "autosize": true, 
+        "title": "Percentages of direct and indirect tweets from Twitter", 
+        "colorway": [
+            "#66c2a5", 
+            "#fc8d62", 
+            "#8da0cb", 
+            "#e78ac3", 
+            "#a6d854", 
+            "#ffd92f", 
+            "#e5c494", 
+            "#b3b3b3"
+        ], 
+        "xaxis": {
+            "range": [
+                -0.5, 
+                4.5
+            ], 
+            "type": "category", 
+            "autorange": true
+        }, 
+        "yaxis": {
+            "range": [
+                0, 
+                290.5263157894737
+            ], 
+            "type": "linear", 
+            "autorange": true
+        }
+    }, 
+    "data": [
+        {
+            "labelssrc": "ksankaseam:8:accb89", 
+            "orientation": "v", 
+            "ysrc": "ksankaseam:8:4dfe45", 
+            "labels": [
+                "Twitter  for Android", 
+                "Twitter for iPad", 
+                "Twitter for iPhone", 
+                "Twitter Media Studio", 
+                "Twitter Web Client", 
+                "Others Application"
+            ], 
+            "xsrc": "ksankaseam:8:accb89", 
+            "marker": {
+                "colors": []
+            }, 
+            "stackgroup": 1, 
+            "values": [
+                "276", 
+                "4", 
+                "195", 
+                "2", 
+                "76", 
+                "215"
+            ], 
+            "mode": "lines", 
+            "y": [
+                "276", 
+                "4", 
+                "195", 
+                "2", 
+                "76", 
+                "215"
+            ], 
+            "x": [
+                "Twitter  for Android", 
+                "Twitter for iPad", 
+                "Twitter for iPhone", 
+                "Twitter Media Studio", 
+                "Twitter Web Client", 
+                "Others Application"
+            ], 
+            "type": "pie", 
+            "valuessrc": "ksankaseam:8:4dfe45"
+        }
+    ]
+}
